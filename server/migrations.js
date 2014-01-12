e119023e7d9ac03c1437ba5f2053de454613aa55
Marek Takac <takacmarek1221@gmail.com>
2014-01-12 17:45:21 +0100

    created basic tasks list page

--- server/migrations.js
+++ server/migrations.js
@@ -2,41 +2,119 @@ Migrations = new Meteor.Collection('migrations');
 
 Meteor.startup(function() {
 
-  if (!Migrations.findOne({name: "createSampleProjects"})) {
-    console.log("//--------------------------------------------------------------------------//")
-    console.log("//------------//    Starting createSampleProjects Migration    //-----------//")
-    console.log("//--------------------------------------------------------------------------//")
-    var projects = [];
+    if (!Migrations.findOne({name: "createSampleProjects"})) {
+        console.log("//--------------------------------------------------------------------------//")
+        console.log("//------------//    Starting createSampleProjects Migration    //-----------//")
+        console.log("//--------------------------------------------------------------------------//")
+        var projects = [];
 
-    projects.push({
-        title: 'Hyaku',
-        description: 'Project managment system for small - medium bussinesses.',
-        slug: 'hyaku',
-        membersNum: 0
-    });
-    projects.push({
-        title: 'Climbing Mount Everest',
-        description: "Don't forget your sweater!",
-        slug: 'climbing-mount-everest',
-        membersNum: 0
-    });
-    projects.push({
-        title: 'Realestate Assistant',
-        description: 'Content managment and administration system for realestate agencies',
-        slug: 'realestate-assistant',
-        membersNum: 0
-    });
+        projects.push({
+            title: 'Hyaku',
+            description: 'Project managment system for small - medium bussinesses.',
+            slug: 'hyaku',
+            tasksNum: {
+                open: 0,
+                close: 0,
+                total: 0
+            },
+            membersNum: 0
+        });
+        projects.push({
+            title: 'Climbing Mount Everest',
+            description: "Don't forget your sweater!",
+            slug: 'climbing-mount-everest',
+            tasksNum: {
+                open: 0,
+                close: 0,
+                total: 0
+            },
+            membersNum: 0
+        });
+        projects.push({
+            title: 'Realestate Assistant',
+            description: 'Content managment and administration system for realestate agencies',
+            slug: 'realestate-assistant',
+            tasksNum: {
+                open: 4,
+                close: 0,
+                total: 4
+            },
+            membersNum: 0
+        });
 
-    _.each(projects, function(project, index) {
-        project = new Project(project);
-        project.save(project);
-        console.log("---------------------");
-        console.log("Creating project #" + (index + 1) + ': ' + project.title);
-    });
+        _.each(projects, function(project, index) {
+            project = new Project(project);
+            project.save(project);
+            console.log("---------------------");
+            console.log("Creating project #" + (index + 1) + ': ' + project.title);
+        });
 
-    Migrations.insert({name: "createSampleProjects"});
-    console.log("//--------------------------------------------------------------------------//")
-    console.log("//------------//     Ending createSampleProjects Migration     //-----------//")
-    console.log("//--------------------------------------------------------------------------//")
-  }
+        Migrations.insert({name: "createSampleProjects"});
+        console.log("//--------------------------------------------------------------------------//")
+        console.log("//------------//     Ending createSampleProjects Migration     //-----------//")
+        console.log("//--------------------------------------------------------------------------//")
+    }
+
+    if (!Migrations.findOne({name: "createSampleTasks"})) {
+        console.log("//--------------------------------------------------------------------------//")
+        console.log("//------------//    Starting createSampleTasks Migration    //-----------//")
+        console.log("//--------------------------------------------------------------------------//")
+        var project = Project.first({slug: 'realestate-assistant'});
+
+        var tasks = [
+            {
+                subject: 'Automation of deployement',
+                description: 'We need to find some way to automate as many deploy tasks as we can. ' +
+                    'Try take a look on http://gruntjs.com/. Seems pretty promising.',
+                priority: 3,
+                project_id: project._id,
+                createdAt: new Date(),
+                updatedAt: null,
+                closedAt: null,
+                status: null
+            },
+            {
+                subject: 'Setup the template',
+                description: '',
+                priority: 2,
+                project_id: project._id,
+                createdAt: new Date(),
+                updatedAt: null,
+                closedAt: null,
+                status: null
+            },
+            {
+                subject: 'Write tests for user roles',
+                description: 'Users roles module should be well tested.',
+                priority: 3,
+                project_id: project._id,
+                createdAt: new Date(),
+                updatedAt: null,
+                closedAt: null,
+                status: null
+            },
+            {
+                subject: 'Lorem ipsum',
+                description: 'Lorem ipsum dolor sit amet consecetur elis.',
+                priority: 1,
+                project_id: project._id,
+                createdAt: new Date(),
+                updatedAt: null,
+                closedAt: null,
+                status: null
+            }
+        ];
+
+        _.each(tasks, function(task, index) {
+            task = new Task(task);
+            task.save(task);
+            console.log("---------------------");
+            console.log("Creating task #" + (index + 1) + ': ' + task.subject);
+        });
+
+        Migrations.insert({name: "createSampleTasks"});
+        console.log("//--------------------------------------------------------------------------//")
+        console.log("//------------//     Ending createSampleTasks Migration     //--------------//")
+        console.log("//--------------------------------------------------------------------------//")
+    }
 });
