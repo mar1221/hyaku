b08456c88b3ada17a72f490de30ec04c63031195
Marek Takac <takacmarek1221@gmail.com>
2014-01-13 16:47:02 +0100

    implement basic server & client side validations

--- client/managers/users/users_new.js
+++ client/managers/users/users_new.js
@@ -1,25 +1,44 @@
-Template.usersNew.events({
-    'submit form': function(e, t) {
-        e.preventDefault();
+Template.usersNew.errors = function() {
+    var errors = Session.get('errors');
+    var validationErrors = {};
 
-        var user = {
-            email: t.find('input#email').value,
-            password: t.find('input#password').value,
-            profile: {
-                firstName: t.find('input#first-name').value,
-                lastName: t.find('input#last-name').value
-            }
+    _.each(errors, function(error) {
+        for (property in error) {
+            validationErrors[property] = validationErrors[property] || []
+            validationErrors[property].push(error[property]);
         };
+    });
 
-        // Trim and validate
+    return validationErrors;
+};
 
-        Meteor.call('addUser', user, function(error) {
-            if (error) {
-                // account creation failed
-            } else {
-                Router.go('/users');
-            }
-        });
+Template.usersNew.events({
+    'submit form': function(event, template) {
+        event.preventDefault();
+
+        var email = _.trim(template.find('input#email').value),
+            firstName = _.trim(template.find('input#first-name').value),
+            lastName = _.trim(template.find('input#last-name').value);
+
+        var user = template.data.user;
+        user.errors = [];
+
+        user.emails = [{ address: email }];
+        user.profile.firstName = firstName;
+        user.profile.lastName = lastName;
+
+        if (user.isValid()) {
+            Meteor.call('addUser', user, function(error) {
+                if (error) {
+                    // account creation failed
+                } else {
+                    Router.go('/users');
+                }
+            });
+        } else {
+            Session.set('errors', user.errors);
+        }
+
         return false;
     }
 });
--- server/methods/users.js
+++ server/methods/users.js
@@ -1,5 +1,19 @@
 Meteor.methods({
     addUser: function(user) {
-        return Accounts.createUser(user);
+        var userId = Accounts.createUser(user);
+        if (!user.password) {
+            Accounts.emailTemplates.siteName = 'Hyaku';
+            Accounts.emailTemplates.from = 'Support <support@hyaku.com>';
+            Accounts.emailTemplates.enrollAccount.subject = function(user) {
+                return "You have been invited to join Hyaku!";
+            };
+            Accounts.emailTemplates.enrollAccount.text = function(user, url) {
+                return 'Welcome ' + user.profile.firstName + ' ' + user.profile.lastName +
+                    '\n\n To ativate your account, please follow the link bellow:\n' +
+                    url;
+            }
+            Accounts.sendEnrollmentEmail(userId);
+        }
+        return userId;
     }
 });
